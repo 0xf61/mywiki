cacc660c4f3e8d6fcfafbae3f2ac482fee0d0691
0xf61 <emirhan@kurt.link>
2025-05-11 19:31:53 +0300

    Squash merge main into ai

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -40,9 +40,10 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Explorer(),
   ],
   right: [
-    Component.Graph(),
+    Component.RecentNotes({ showTags: false }),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
+    Component.Graph(),
   ],
 }
 
